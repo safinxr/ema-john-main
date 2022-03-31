a362192e998628d6e669677b16df5e709055fedd
safinxr <safinxr@gmail.com>
2022-03-31 13:52:16 +0600

    gg

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="/shop">Shop</a>
+                <a href="/shop">ShoP</a>
                 <a href="/orders">Orders</a>
                 <a href="/inventory">Inventory</a>
                 <a href="/about">About</a>
