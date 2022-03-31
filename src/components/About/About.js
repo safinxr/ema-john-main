dba27f59c669d05603c9d7353148d92950a76ae4
safinxr <safinxr@gmail.com>
2022-03-31 20:24:58 +0600

    m3

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div>
+            <h1>sorry you cont know about us</h1>
+        </div>
+    );
+};
+
+export default About;
