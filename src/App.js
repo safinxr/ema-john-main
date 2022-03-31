1f34216afa094228ef7fcddf7b891556c03ae94b
Safinxr <safinxr@gmail.com>
2022-04-01 00:57:34 +0600

    4

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
-        <Route path='./about' element={<About></About>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './Orders.css'
+import React from "react";
+import useCart from "../../Hooks/UseCart";
+import useProduct from "../../Hooks/UseProducts";
+import "./Orders.css";
 
 const Orders = () => {
-    return (
-        <div>
-            <h1>This is Order</h1>
-        </div>
-    );
+  const [products, setProducts] = useProduct();
+  const [cart, setCart] = useCart(products);
+  return (
+    <div>
+      <h1>This is Order : {products.length}</h1>
+      <p>Cart: {cart.length}</p>
+    </div>
+  );
 };
 
 export default Orders;
